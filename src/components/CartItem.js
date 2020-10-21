da777fc47bc03ccf1a16b25abd63f49c19b27258
khagag <the_lord29@yahoo.com>
2020-10-21 14:28:30 +0200

    [add] listItems grid

---
+++ src/components/CartItem.js
@@ -0,0 +1,52 @@
+import React ,{Component} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function CartItem() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://i.pinimg.com/736x/34/df/ee/34dfeed20d644ba572bd2d8d31bc8d77.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button variant="contained" xs color="primary">
+          Add to cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,27 @@
 import React ,{Component} from 'react';
 import HeaderNav from '../components/HeaderNav';
+import CartItem from '../components/CartItem';
+import {Grid} from '@material-ui/core';
+
 
 class Home extends Component{
   render(){
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number) =>
+      <Grid item xs={3}>
+        <CartItem/>
+      </Grid>
+    );
     return(
       <div>
         <HeaderNav/>
+        <br/>
+        <Grid container justify="center" spacing={2} style={{
+    margin: 0,
+    width: '100%',
+  }}>
+          {listItems}
+        </Grid>
       </div>
     )
   }
