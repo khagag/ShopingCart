802ea38e729c8e9858c67c32dc9fa0152dd6c8eb
khagag <the_lord29@yahoo.com>
2020-10-22 03:49:54 +0200

    [modify] get items list dynamicaly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,41 @@ import React ,{Component} from 'react';
 import HeaderNav from '../components/HeaderNav';
 import CartItem from '../components/CartItem';
 import {Grid} from '@material-ui/core';
+const axios = require('axios');
 
 
 class Home extends Component{
+  constructor(props) {
+      super(props);
+      this.state = {listItems: ''};
+    }
+  componentDidMount() {
+      axios.get(`http://localhost/api/goods`)
+      .then(res => {
+        let l = res.data.map((item) =>
+            <Grid item xs={3}>
+              <CartItem/>
+            </Grid>
+          );
+          this.setState({listItems:l});
+        console.log(
+          this.listItems
+        );
+      })
+    }
+
   render(){
-  const numbers = [1, 2, 3, 4, 5];
-  const listItems = numbers.map((number) =>
-      <Grid item xs={3}>
-        <CartItem/>
-      </Grid>
-    );
+  console.log(status);
+  // const numbers = [1, 2, 3, 4, 5];
     return(
       <div>
         <HeaderNav/>
         <br/>
         <Grid container justify="center" spacing={2} style={{
-    margin: 0,
-    width: '100%',
-  }}>
-          {listItems}
+            margin: 0,
+            width: '100%',
+          }}>
+          {this.state.listItems}
         </Grid>
       </div>
     )
