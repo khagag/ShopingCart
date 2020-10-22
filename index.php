12fa03ebd006ec13bc687ede6aa930af156c96bd
khagag <the_lord29@yahoo.com>
2020-10-22 04:55:35 +0200

    [add] checkout route

--- index.php
+++ index.php
@@ -17,20 +17,41 @@ function abort($state){
 //routing
 // var_dump($parameters);
 if($parameters[1]=='api'){
+  $data = [
+    ['name'=>'T-shirt ','price'=>10.99,'id'=>0],
+    ['name'=>'Pants','price'=>14.99,'id'=>1],
+    ['name'=>'Jacket','price'=>19.99,'id'=>2],
+    ['name'=>'Shoes','price'=>24.99,'id'=>3],
+  ];
   switch ($parameters[2]) {
     case 'goods':
       // code...
       respond(
-        json_encode([
-          ['name'=>'T-shirt ','price'=>10.99,'id'=>1],
-          ['name'=>'Pants','price'=>14.99,'id'=>2],
-          ['name'=>'Jacket','price'=>19.99,'id'=>3],
-          ['name'=>'Shoes','price'=>24.99,'id'=>4],
-        ]));
+        json_encode($data));
       break;
     case 'checkout':
-      if($_SERVER['REQUEST_METHOD']== 'POST')
-        respond(json_encode([]));
+      $subtotal = $s_discount ,$t_discount,$total = 0.0;
+      if($_SERVER['REQUEST_METHOD']== 'POST'){
+        for($_POST['items'] as $item){
+          $subtotal+=$item->count * $data[$item->id]->price;
+          if($data[$item->id] == 3){
+            $s_discount+=$item->count * $data[$item->id]->price * 0.1;
+          }else if($data[$item->id] == 0){
+            $m = floor($data[$item->id] / 3);
+            $t_discount += $m * $data[$item->id];
+          }
+        }
+      }
+      $tax = $subtotal * 0.14;
+      $total = $subtotal - $s_discount - $t_discount - $tax;
+        respond(json_encode([
+          'success'=>true,
+          'total'=>$total,
+          'subtotal'=>$subtotal,
+          's_discount'=>$s_discount,
+          't_discount'=>$t_discount,
+          'tax' => $tax
+        ]));
       break;
     default:
       require('./src/views/index.html');
